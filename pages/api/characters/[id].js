ae1810d9ca7977bd12aa1c207cc90b09b61a46e1
Scott Brabson <scottbrabson@gmail.com>
2021-04-08 16:46:11 -0600

    add logic to check property type

--- pages/api/characters/[id].js
+++ pages/api/characters/[id].js
@@ -1,3 +1,5 @@
+import { fetchQuery, baseUrl } from "../../../utils/fetchQuery";
+
 export default async (req, res) => {
   const { id } = req.query;
 
@@ -20,5 +22,40 @@ export default async (req, res) => {
 };
 
 const buildCharacterDetails = (character) => {
-  return character
+  // get properties w/endpoints
+  // get properties w/array of endpoints
+  const {homeworld, films, species, vehicles, starships} = character
+  const endpoints = [homeworld, films, species, vehicles, starships]
+
+  const fetchedPropertyData = endpoints.map(property => {
+    if (checkType(property)) {
+
+    }
+  })
+
+  console.log(fetchedPropertyData)
+
+  let compiled = character
+
+  if (compiled.homeworld) {
+    const homeworld = "test"
+    compiled.homeworld = homeworld
+  }
+
+  console.log(compiled.homeworld)
+
+  return compiled
+}
+
+const checkType = (property) => {
+  if (Array.isArray(property) && property.length) {
+    console.log('valid array of endpoints')
+    return "array"
+  } else if (typeof property === 'string' && property.split('/')[0] === 'http:') {
+    console.log('valid single endpoint')
+    return "string"
+  } else {
+    console.log('no endpoints')
+    return false
+  }
 }
