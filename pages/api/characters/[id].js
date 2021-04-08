21c818587f8f719e4d7ef314783c43dd99484af3
Scott Brabson <scottbrabson@gmail.com>
2021-04-08 16:09:07 -0600

    add helper function to build JSON object

--- pages/api/characters/[id].js
+++ pages/api/characters/[id].js
@@ -6,10 +6,19 @@ export default async (req, res) => {
       `https://swapi.dev/api/people/${id}`
     );
     const data = await request.json();
+
+    
+    const compiledData = buildCharacterDetails(data)
+
+
     res.setHeader("Content-Type", "application/json");
-    res.status(200).json({ data })
+    res.status(200).json({ data: compiledData })
     res.end();
   } catch (e) {
     res.status(400).end();
   }
 };
+
+const buildCharacterDetails = (character) => {
+  return character
+}
