174be19f64eb415e61964a4ce17d45bbad2f6111
Scott Brabson <scottbrabson@gmail.com>
2021-04-09 17:35:37 -0600

    fix typo for staging env var

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ module.exports = (phase) => {
       if (isProd) {
         return 'https://swapi-challenge-fe.vercel.app/api'
       }
-      if (isStaging) return 'swapi-challenge-fe-staging.vercel.app/api'
+      if (isStaging) return 'https://swapi-challenge-fe-staging.vercel.app/api'
       return 'RESTURL_SPEAKERS:not (isDev,isProd && !isStaging,isProd && isStaging)'
     })()
   }
