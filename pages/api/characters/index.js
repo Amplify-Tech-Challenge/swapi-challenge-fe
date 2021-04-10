9b39a283216bed63d535ba87965bde082e77d249
Scott Brabson <scottbrabson@gmail.com>
2021-04-09 18:30:58 -0600

    more bug hunting

--- pages/api/characters/index.js
+++ pages/api/characters/index.js
@@ -4,7 +4,7 @@ export default async (req, res) => {
   try {
     const data = await getCharacters("test");
     res.setHeader("Content-Type", "application/json");
-    res.status(200).json(JSON.stringify({ data }));
+    res.status(200).json({ data });
     res.end();
   } catch (e) {
     res.status(400).end();
