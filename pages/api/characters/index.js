8ab1afca191e576fed0c5cc75db084134dbfc8d3
Scott Brabson <scottbrabson@gmail.com>
2021-04-09 18:32:41 -0600

    more hunting

--- pages/api/characters/index.js
+++ pages/api/characters/index.js
@@ -1,12 +1,13 @@
 import getCharacters from "../../../utils/getCharacters";
 
 export default async (req, res) => {
-  try {
-    const data = await getCharacters("test");
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).json({ data });
-    res.end();
-  } catch (e) {
-    res.status(400).end();
-  }
+  // try {
+  //   const data = await getCharacters("test");
+  //   res.setHeader("Content-Type", "application/json");
+  //   res.status(200).json({ data });
+  //   res.end();
+    res.status(200);
+  // } catch (e) {
+  //   res.status(400).end();
+  // }
 };
