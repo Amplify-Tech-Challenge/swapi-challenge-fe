6d4b5767cebf1d41bb1dd84d2917def298b487d4
Scott Brabson <scottbrabson@gmail.com>
2021-04-09 18:34:30 -0600

    invalid json

--- pages/api/characters/index.js
+++ pages/api/characters/index.js
@@ -1,13 +1,15 @@
-import getCharacters from "../../../utils/getCharacters";
+// import getCharacters from "../../../utils/getCharacters";
 
 export default async (req, res) => {
-  // try {
+  try {
   //   const data = await getCharacters("test");
   //   res.setHeader("Content-Type", "application/json");
   //   res.status(200).json({ data });
   //   res.end();
-    res.status(200);
-  // } catch (e) {
-  //   res.status(400).end();
-  // }
+    res.status(200).end();
+  } catch (e) {
+    res.status(400).end();
+  }
 };
+
+// which part of this is invalid json?
