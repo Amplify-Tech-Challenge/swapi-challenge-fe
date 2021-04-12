abd727d23c6254bb54f0725a12e848c7c995c40a
Scott Brabson <scottbrabson@gmail.com>
2021-04-12 16:49:39 -0600

    implement styled components

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,23 +1,33 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const SearchContainer = styled.div`
+  background: red;
+`
+const SearchInput = styled.input`
+  width: 100%;
+  height: 3em;
+`
 
 const SearchBar = () => {
   const [input, setInput] = useState("")
 
   const handleChange = (searchTerm) => {
-    setInput(searchTerm.value)
+    setInput(searchTerm)
+    console.log(searchTerm)
   }
 
   return ( 
-    <div>
-      <input 
+    <SearchContainer>
+      <SearchInput 
         type="text"
         placeholder="Search characters by name"
         aria-label="Character Search Bar"
         name="input"
         value={input}
-        onChange={event => handleChange(event.target)}
+        onChange={event => handleChange(event.target.value)}
       />
-    </div>
+    </SearchContainer>
    );
 }
  
