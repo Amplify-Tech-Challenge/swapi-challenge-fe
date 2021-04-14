63bedddb9c3e34869a7e50995ec7aed0baa7446e
Scott Brabson <scottbrabson@gmail.com>
2021-04-14 14:28:54 -0600

    convert to ul/li for accessibility

--- pages/characters/index.js
+++ pages/characters/index.js
@@ -1,63 +1,88 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import CharacterCard from "../../components/CharacterCard/CharacterCard";
-const API_URL = process.env.RESTURL_MYAPI;
 import styles from "../../styles/Home.module.css";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import styled from "styled-components";
+const API_URL = process.env.RESTURL_MYAPI;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  background: gray;
+  `;
+const Results = styled.ul`
+  display: flex;
+  flex-direction: column;
+  flex-flow: wrap;
+  margin-top: 0;
+  background: gray;
+  list-style-type: none;
+  align-items: center
+  justify-content: center;
+  padding: 0;
+`;
+const Title = styled.span`
+  background: inherit;
+`
 
 const CharacterList = () => {
-  const [searchResults, setSearchResults] = useState([])
-  const [noResults, setNoResults] = useState(null)
-  const [previousSearchResults, setPreviousSearchResults] = useState([])
+  const [searchResults, setSearchResults] = useState([]);
+  const [previousSearchResults, setPreviousSearchResults] = useState([]);
+  const [noResults, setNoResults] = useState(null);
 
-  const parseResults = (resultObject) => {
-    setPreviousSearchResults(searchResults)
-    setSearchResults(resultObject?.results)
-    resultObject.message ? setNoResults(resultObject.message) : setNoResults(null)
-  }
+  const parseResults = resultObject => {
+    setPreviousSearchResults(searchResults);
+    setSearchResults(resultObject.results);
+    resultObject.message
+      ? setNoResults(resultObject.message)
+      : setNoResults(null);
+  };
 
   const makeCharacterList = () => {
+    console.log('list fire')
     const getId = url => {
       const splitUrl = url.split("/");
       const id = splitUrl[splitUrl.length - 2];
       return id;
     };
 
-    if (searchResults?.length) {
+    // if (previousSearchResults.length && !searchResults) {
+    //   return previousSearchResults.map(c => {
+    //     const id = getId(c.url);
+    //     const characterData = { ...c, id };
+    //     return <CharacterCard key={Math.random()} character={characterData} />;
+    //   });
+    // } else
+    if (searchResults.length) {
       return searchResults.map(c => {
         const id = getId(c.url);
         const characterData = { ...c, id };
-        return <CharacterCard key={Math.random()} character={characterData} />;
+        return <li><CharacterCard key={Math.random()} character={characterData} /></li>;
       });
     }
   };
 
   const showSearchState = () => {
-
-    return <h1 className={styles.title}>{searchResults?.length ? 'Results' : noResults ? noResults : "Search by name above"}</h1>
-  }
+    if (noResults) {
+      return <h1 className={styles.title}>{noResults}</h1>;
+    } else if (previousSearchResults.length && !searchResults.length) {
+      return <h1 className={styles.title}>Results</h1>;
+    } else if (!previousSearchResults.length && !searchResults.length) {
+      return <h1 className={styles.title}>Search by name above</h1>;
+    } else {
+      return <h1 className={styles.title}>Results</h1>;
+    }
+  };
 
   return (
-    <>
-      <SearchBar getResults={parseResults}/>
-      <div className={styles.container}>
-        {/* TODO break out into separate function to add 'loading'  */}
-        {showSearchState()}
+    <Main>
+      <SearchBar getResults={parseResults} />
+      <Title>{showSearchState()}</Title>
+      <Results >     
         {makeCharacterList()}
-      </div>
-    </>
+      </Results>
+    </Main>
   );
 };
-
-// export const getStaticProps = async () => {
-//   // will load up a page of movies, can append more with 'load more'
-//   // search field will make api call and load SW card components with information/cancel previously typed letter
-
-//   const request = await fetch(`${API_URL}/characters/`);
-//   const data = await request.json();
-
-//   return {
-//     props: { characters: data },
-//   };
-// };
 
 export default CharacterList;
