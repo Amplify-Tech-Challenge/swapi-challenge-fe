9c20243ab4278687ac915c432f86460db278bb1e
Scott Brabson <scottbrabson@gmail.com>
2021-04-15 16:55:28 -0600

    make call to gateway not to SWAPI

--- utils/apiCalls.js
+++ utils/apiCalls.js
@@ -1,12 +1,12 @@
+const API_URL = process.env.RESTURL_MYAPI;
+
 export const fetchLiveSearch = async (query, updatedPageNo = "") => {
   const pageNo = updatedPageNo ? updatedPageNo : "";
-  const apiEndpoint = `https://swapi.py4e.com/api/people/?search=${query}&page=${pageNo}`;
+  const apiEndpoint = `${API_URL}/characters/?search=${query}&page=${pageNo}`;
   
   try {
     const response = await fetch(apiEndpoint);
-    const data = await response.json();
-    const results = data.results;
-
+    const results = await response.json();
     if (results) {
       const stateObject = {
         results,
